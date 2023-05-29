a43cd66cf9c64305633fb44b0d5ceff593b6dc21
Singhx <126919241+singhx-hub@users.noreply.github.com>
2023-05-29 05:56:55 +0100

    bug fix in main

---
+++ PassArmor.py
@@ -0,0 +1,111 @@
+import tkinter as tk
+from tkinter import *
+from colorama import Fore, Style
+from termcolor import colored
+
+# GUI design
+root = Tk()
+root.title("PassArmor: Safeguarding Your Digital Identity")
+root.resizable(False, False)
+root.geometry("700x500+500+200")
+root.configure(bg="black")
+
+# Icon
+icon = PhotoImage(file="logo.png")
+root.iconphoto(True, icon)
+
+# Logo
+logo = PhotoImage(file="logo1.png")
+Label(root, image=logo, bg="black").place(x=175, y=7)
+Label(root, text="PassArmor", bd=3, bg="black", fg="red", font="arial 30 bold").place(x=270, y=20)
+Label(root, text="Safeguarding Your Digital Identity", bg="black", fg="white", font="arial 7").place(x=300, y=65)
+Label(root, text="\" Check password's strength and ensure it hasn't been publicly leaked \"", bd=3, bg="black",
+      fg="yellow", font="arial 10 bold").place(x=120, y=110)
+
+# Password label
+pass_label = tk.Label(root, text="Enter Password:", bd=5, fg="white", bg="black", font="arial 15 bold")
+pass_label.place(x=140, y=170)
+
+# Text box
+text_box = tk.Entry(root, show="*", bd=5, fg="black", bg="white", font="arial 15 bold")
+text_box.place(x=310, y=170)
+
+# Check button
+check_button = tk.Button(root, text="Check", font="arial 15 bold", command=lambda: check_password(text_box.get()))
+check_button.place(x=260, y=250)
+
+# Show button
+show_button = tk.Button(root, text="Show ", font="arial 15 bold")
+show_button.place(x=350, y=250)
+
+# Result label
+result_label = tk.Label(root, text="", bd=5, bg="white", font="arial 10 bold")
+result_label.place(x=230, y=330)
+
+
+# Check for password Strength
+def check_strength(password):
+    min_len = 8
+    number = False
+    letter = False
+    mix_char = False
+
+    if len(password) >= min_len:  # check for length
+        for char in password:
+            if char.isdigit():  # check for digits
+                number = True
+                break
+
+        for char in password:
+            if char.isalpha():  # check for letters
+                letter = True
+                break
+
+        for char in password:
+            if char.isalnum():  # check mixture for digits, special characters and numbers
+                mix_char = True
+                break
+
+    if len(password) < min_len:
+        strength = "\u26A0 Weak: Password is too short."
+
+    elif not number:
+        strength = "\u26A0 Weak: Password must contain at least one number."
+
+    elif not letter:
+        strength = "\u26A0 Weak: Password must contain at least one letter"
+
+    elif not mix_char:
+        strength = "\u26A0 Weak: Password must contain letters, numbers & special characters.}"
+
+    else:
+        strength = "\u2713 Strong: Password meets all strength criteria."
+
+    return strength
+
+
+# Check for password leakage
+def check_password_leakage(password):
+    file = open("leak.txt", "r")  # Open file in reading mode
+    leaked_passwords = file.readlines()  # Read file content
+    leaked_passwords = [line.strip() for line in leaked_passwords]  # Remove the newline character from each line and store in a new list
+
+    if password in leaked_passwords:
+        return "\u26A0 CAUTION: Password is publicly leaked."
+
+    else:
+        return "\u2713 Password is good to use."
+
+    file.close()
+
+
+# Integrating Password Strength and Leakage Checks
+def check_password(password):
+    strength_result = check_strength(password)
+    leakage_result = check_password_leakage(password)
+
+    result_message = "{}\n \n {}".format(strength_result, leakage_result)
+    result_label.configure(text=result_message)
+
+
+root.mainloop()
